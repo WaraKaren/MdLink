35a473cdd6159a51c57a5a2995ff274430d27e20
WaraKaren <wkpacajesm@gmail.com>
2023-11-21 13:48:57 -0400

    se termino de realizar los test del hito1

--- test/md-links.spec.js
+++ test/md-links.spec.js
@@ -1,26 +1,46 @@
-const {isMdwnExtension} = require('./../lib/markdown.js');
+const { isMdwnExtension } = require("./../lib/markdown.js");
 
-
-describe('isMdwnExtension', () => {
-
-  it('deberia devolver true si la ruta es un archivo md', () => {
-   const path = "C:/Users/Hewlett Packard/Desktop/LABORATORIA CODIGO/DEV010-md-links/README.md"
-   expect (isMdwnExtension(path)).toBe(true)
+describe("isMdwnExtension", () => {
+  it("deberia devolver true si la ruta es un archivo md", () => {
+    const path =
+      "C:/Users/Hewlett Packard/Desktop/LABORATORIA CODIGO/DEV010-md-links/README.md";
+    expect(isMdwnExtension(path)).toBe(true);
   });
-it('Deberia devolver false si la ruta es un archivo md',() => {
-  const path = "C:/Users/Hewlett Packard/Desktop/LABORATORIA CODIGO/DEV010-md-links/thumb.png"
-  expect (isMdwnExtension(path)).toBe(false)
+  it("Deberia devolver false si la ruta es un archivo md", () => {
+    const path =
+      "C:/Users/Hewlett Packard/Desktop/LABORATORIA CODIGO/DEV010-md-links/thumb.png";
+    expect(isMdwnExtension(path)).toBe(false);
+  });
 });
+
+//FIXME: test de ReadFile
+const fReadFile = require("./../lib/funReadFile.js");
+
+describe("fReadFile", () => {
+  it.only('debería leer el contenido de un archivo', () => {
+    const archivo = 'C:/Users/Hewlett Packard/Desktop/LABORATORIA CODIGO/DEV010-md-links/examples/ejemploTest.md';
+    const contenidoEsperado =  "Google  [Enlace a Google](https://www.google.com).";
+// expect(fReadFile(archivo)).toBe('Google')
+// console.log(fReadFile(archivo))
+return fReadFile(archivo).then(data => {
+  expect(data).toBe(contenidoEsperado);
 });
+  });
+});
+
+
 
 //FIXME: test para la funcion para extraer links
 
-const links = require('./../lib/funLiks.js');
+const links = require("./../lib/funLiks.js");
 
-describe('links', () => {
-  it('debería retornar un array de objetos con los enlaces encontrados', () => {
-    const contenido = 'ejemplo de un enlace [enlace](https://www.ejemplo.com) en un texto.';
-    const resultadoEsperado = [{ titulo: 'enlace', url: 'https://www.ejemplo.com' }];
+describe("links", () => {
+  it("debería retornar un array de objetos con los enlaces encontrados", () => {
+    const contenido =
+      "ejemplo de un enlace [enlace](https://www.ejemplo.com) en un texto.";
+    const resultadoEsperado = [
+      { titulo: "enlace", url: "https://www.ejemplo.com" },
+    ];
 
     expect(links(contenido)).toEqual(resultadoEsperado);
   });
